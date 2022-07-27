089e40b83ff29fdbcc3af17cf2a0ac78cad15936
Glady Barrios <Barrios.glady01@gmail.com>
2022-07-26 22:39:26 -0500

    dding some last edits

--- prepare.py
+++ prepare.py
@@ -3,6 +3,7 @@ import env
 import numpy as np
 from wrangle import wrangle_zillow
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 def train_validate_test_split(df):
     '''
@@ -15,3 +16,22 @@ def train_validate_test_split(df):
                                    random_state=123,  stratify=train_validate.fips)
 
     return train, validate, test
+
+
+def scale_data(train, validate, test):
+    
+    scale_columns = ['bedroomcnt', 'bathroomcnt', 'calculatedfinishedsquarefeet']
+    
+    train_scaled = train.copy()
+    validate_scaled = validate.copy()
+    test_scaled = test.copy()
+    
+    mms = MinMaxScaler()
+    
+    mms.fit(train[scale_columns])
+    
+    train_scaled[scale_columns] = mms.transform(train[scale_columns])
+    validate_scaled[scale_columns] = mms.transform(validate[scale_columns])
+    test_scaled[scale_columns] = mms.transform(test[scale_columns])
+    
+    return train_scaled, validate_scaled, test_scaled
